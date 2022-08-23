b364945599e651202ed89fa6f8d3138588edbe69
saint <saintmoses100@gmail.com>
2022-08-23 08:58:46 -0300

    navbar

---
+++ templates/header.php
@@ -0,0 +1,32 @@
+<head>
+
+    <title>Document</title>
+ <!--Import Google Icon Font-->
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import materialize.css-->
+      <link rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+      <style type="text/css">
+        .brand{
+            background: #cbb09c !important;
+
+        }
+        .brand-text{
+            color:#cbb09c !important;
+        }
+      </style>
+</head>
+ <body class = 'gray lighten-4'>
+    <nav class="white z-depth-0">
+        <div class="container">
+            <a href="#" class="brand-logo brand-text">PIZZA</a>
+            <ul id ="nav-mobile" class=""right hide-on-small-and-down>
+                <li> <a href="#" class="btn brand z-depth-0">Add a Pizza</a> </li>
+            </ul>
+        </div>
+    </nav>
+
+    
