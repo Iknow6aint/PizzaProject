a1b439ca907f912cc0e9489083c11cbae4dec1a4
saint <saintmoses100@gmail.com>
2022-08-23 09:05:57 -0300

    navbar refactor

--- templates/header.php
+++ templates/header.php
@@ -1,13 +1,7 @@
 <head>
 
     <title>Document</title>
- <!--Import Google Icon Font-->
-      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-      <!--Import materialize.css-->
-      <link rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
-
-      <!--Let browser know website is optimized for mobile-->
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 
       <style type="text/css">
         .brand{
@@ -19,14 +13,12 @@
         }
       </style>
 </head>
- <body class = 'gray lighten-4'>
-    <nav class="white z-depth-0">
-        <div class="container">
-            <a href="#" class="brand-logo brand-text">PIZZA</a>
-            <ul id ="nav-mobile" class=""right hide-on-small-and-down>
-                <li> <a href="#" class="btn brand z-depth-0">Add a Pizza</a> </li>
-            </ul>
-        </div>
-    </nav>
-
-    
+    <body class="grey lighten-4">
+	<nav class="white z-depth-0">
+    <div class="container">
+      <a href="#" class="brand-logo brand-text">Pizza</a>
+      <ul id="nav-mobile" class="right hide-on-small-and-down">
+        <li><a href="#" class="btn brand z-depth-0">Add a Pizza</a></li>
+      </ul>
+    </div>
+  </nav>
