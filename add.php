c27c72dbae5afcb755c38acd193ccb2b6816e7ee
saint <saintmoses100@gmail.com>
2022-08-25 07:43:44 -0300

    get and post

--- add.php
+++ add.php
@@ -1,6 +1,10 @@
 <?php
 
-
+if(isset($_POST['sbumit'])){
+    echo $_POST['email'];
+    echo $_POST['ingredients'];
+    echo $_POST['title'];
+}
 
 
 ?>
@@ -12,8 +16,19 @@
 
  <section class="container grey-text">
     <h4 class="center">Add a Pizza</h4>
+     <form class="white" action="add.php" method="POST">
+        <Label>YOUR EMAIL</Label>
+        <input type="text" name="email">
+        <Label>Pizza TItlle</Label>
+        <input type="text" name="title">
+        <Label>Ingredients(comma separated):</Label>
+        <input type="text" name="ingredients">
+        <div class= "center">
+            <input type="submit" name="submit" value="submit" class="btn brand z-depth-0">
+        </div>
+     </form>
  </section>
- <form class="white" action="" m></form>
+
 
 
  <?php include 'templates/foooter.php';?>
