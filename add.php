6cd350e6ce367986dc1e03d8e7529aaef5a40fcb
saint <saintmoses100@gmail.com>
2022-08-26 10:04:51 -0300

    show errors

--- add.php
+++ add.php
@@ -1,37 +1,42 @@
 <?php
 
-if(isset($_POST['sbumit'])){
-    //check email
-    if(empty($_POST['email'])){
-        echo 'An email is required <br />';
-    }else{
-        $email = $_POST['email'];
-        if(!filter_var($email,FILTER_VALIDATE_EMAIL)){
-            echo 'EMAI must be valid';
-        }
-    }
+$email = $title = $ingredients = '';
+	$errors = array('email' => '', 'title' => '', 'ingredients' => '');
 
-    //check title
-        if(empty($_POST['title'])){
-        echo 'An title is required <br />';
-    }else{
-        $title = $_POST['title'];
-        if(!preg_match('/^[a-zA-Z/s]+$/',$title)){
-            echo 'TItle must be letters and space only';
-        }
-    }
-
-        if(empty($_POST['ingredients'])){
-        echo 'An ingredient is required <br />';
-    }else{
-        $ingredients = $_POST['ingredients'];
-       if(!preg_match('/^([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/', $ingredients)){
-				echo 'Ingredients must be a comma separated list';
+	if(isset($_POST['submit'])){
+		
+		// check email
+		if(empty($_POST['email'])){
+			$errors['email'] = 'An email is required';
+		} else{
+			$email = $_POST['email'];
+			if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+				$errors['email'] = 'Email must be a valid email address';
 			}
-    }
- 
+		}
 
-}
+		// check title
+		if(empty($_POST['title'])){
+			$errors['title'] = 'A title is required';
+		} else{
+			$title = $_POST['title'];
+			if(!preg_match('/^[a-zA-Z\s]+$/', $title)){
+				$errors['title'] = 'Title must be letters and spaces only';
+			}
+		}
+
+		// check ingredients
+		if(empty($_POST['ingredients'])){
+			$errors['ingredients'] = 'At least one ingredient is required';
+		} else{
+			$ingredients = $_POST['ingredients'];
+			if(!preg_match('/^([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/', $ingredients)){
+				$errors['ingredients'] = 'Ingredients must be a comma separated list';
+			}
+		}
+
+	} // end POST check
+
 
 ?>
 
@@ -44,11 +49,20 @@ if(isset($_POST['sbumit'])){
     <h4 class="center">Add a Pizza</h4>
      <form class="white" action="add.php" method="POST">
         <Label>YOUR EMAIL</Label>
-        <input type="text" name="email">
+        <input type="text" name="email" value ="<?php echo htmlspecialchars($email) ?>">
+        <div class="red-text">
+            <?php echo $errors['email'] ?>
+        </div>
         <Label>Pizza TItlle</Label>
-        <input type="text" name="title">
+        <input type="text" name="title" value ="<?php echo htmlspecialchars($title)?>">
+                <div class="red-text">
+            <?php echo $errors['title'] ?>
+        </div>
         <Label>Ingredients(comma separated):</Label>
-        <input type="text" name="ingredients">
+        <input type="text" name="ingredients" value ="<?php echo htmlspecialchars($ingredients) ?>">
+                <div class="red-text">
+            <?php echo $errors['ingredients'] ?>
+        </div>
         <div class= "center">
             <input type="submit" name="submit" value="submit" class="btn brand z-depth-0">
         </div>
