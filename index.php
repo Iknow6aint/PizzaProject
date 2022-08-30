c99cfdc28d5b403e937621db20468d2696a2bffb
saint <saintmoses100@gmail.com>
2022-08-30 08:30:00 -0300

    database conncet

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 
 <?php
+//connncetDB
+$conn = mysqli_connect('localhost','iknowsaint','iknowsaint','pizza projeect');
 
+//check conneection
+if(!$conn){
+    echo 'connection error:' . mysqli_connect_error();
+}
 
 
 
