d87da4b70991456cdaf0f6afe98a463eeb3d60ab
saint <saintmoses100@gmail.com>
2022-08-30 20:00:19 -0300

    output db

--- index.php
+++ index.php
@@ -8,21 +8,21 @@ if(!$conn){
     echo 'connection error:' . mysqli_connect_error();
 }
 //write query
-$sql = 'SELECT * FROM pizzas';
+$sql = 'SELECT title, ingredients, id FROM pizzas ORDER BY created_at';
 
 // make query & get results
-$results = mysqli_query($conn,$sql);
+$result = mysqli_query($conn,$sql);
 
 // fethc the resulting rows as an array
-$pizzas = mysqli_fetch_all($results,MYSQLI_ASSOC);
+$pizzas = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
 // free result from memory
-mySqli_free_result($results);
+mysqli_free_result($result);
 
 //close connection
 mysqli_close($conn);
 
-print_r($pizzas)
+//print_r($pizza)
 
 ?>
 
@@ -30,6 +30,29 @@ print_r($pizzas)
 <html lang="en">
 
  <?php include 'templates/header.php'; ?>
+<h4 class="center grey-text">Pizzas</h4>
+<div class="container">
+		<div class="row">
+
+			<?php foreach($pizzas as $pizza){ ?>
+
+				<div class="col s6 md3">
+					<div class="card z-depth-0">
+						<div class="card-content center">
+							<h6><?php echo htmlspecialchars($pizza['title']); ?></h6>
+							<div><?php echo htmlspecialchars($pizza['ingredients']); ?></div>
+						</div>
+						<div class="card-action right-align">
+							<a class="brand-text" href="#">more info</a>
+						</div>
+					</div>
+				</div>
+
+			<?php } ?>
+
+		</div>
+	</div>
+
  <?php include 'templates/foooter.php';?>
 
 </body>
