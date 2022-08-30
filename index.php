176b86aa6e04625fc1c654dc8c889e5ccf6ca8fd
saint <saintmoses100@gmail.com>
2022-08-30 18:21:16 -0300

    database conncet

--- index.php
+++ index.php
@@ -7,8 +7,22 @@ $conn = mysqli_connect('localhost','iknowsaint','iknowsaint','pizza projeect');
 if(!$conn){
     echo 'connection error:' . mysqli_connect_error();
 }
+//write query
+$sql = 'SELECT * FROM pizzas';
 
+// make query & get results
+$results = mysqli_query($conn,$sql);
 
+// fethc the resulting rows as an array
+$pizzas = mysqli_fetch_all($results,MYSQLI_ASSOC);
+
+// free result from memory
+mySqli_free_result($results);
+
+//close connection
+mysqli_close($conn);
+
+print_r($pizzas)
 
 ?>
 
