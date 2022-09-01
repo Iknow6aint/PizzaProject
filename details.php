1e4b8de8e537889b0ffff2c6c5ef1d840b5b9839
iknow6aint <saintagbukor@gmail.com>
2022-08-31 21:54:04 -0300

    get recored

---
+++ details.php
@@ -0,0 +1,45 @@
+<?php
+
+include('config/db_connect.php');
+
+//check GET REQUEST ID
+if(isset($_GET['id'])){
+
+
+    $id = mysqli_real_escape_string($conn, $_GET['id']);
+
+    //make sql query
+    $sql = "SELECT * FROM pizzas WHERE id = $id";
+
+    //get query result
+     
+    $result = mysqli_query($conn,$sql);
+
+    //fetch result from array
+    $pizza = mysqli_fetch_assoc($result);
+
+    mysqli_free_result($result);
+    mysqli_close($conn); 
+}
+
+
+?>
+
+<html lang="en">
+ <?php include 'templates/header.php'; ?>
+
+ <div class="container center">
+		<?php if($pizza): ?>
+
+			<h4><?php echo $pizza['title']; ?></h4>
+			<p>Created by <?php echo $pizza['email']; ?></p>
+			<p><?php echo date($pizza['created_at']); ?></p>
+			<h5>Ingredients:</h5>
+			<p><?php echo $pizza['ingredients']; ?></p>
+		<?php else: ?>
+			<h5>No such pizza exists.</h5>
+		<?php endif ?>
+	</div>
+ <?php include 'templates/foooter.php';?>
+
+</html>
